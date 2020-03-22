84adeb25cea60da86c7fe32ffb4b3f57a78c8d41
yangziwen <yangziwen@meituan.com>
2020-03-22 08:18:19 +0800

    fix

--- src/main/frontend/webpack.prod.config.js
+++ src/main/frontend/webpack.prod.config.js
@@ -6,7 +6,7 @@ const webpackBaseConfig = require('./webpack.base.config.js');
 const fs = require('fs');
 
 fs.open('./src/config/env.js', 'w', function (err, fd) {
-    const buf = 'export default "production";';
+    const buf = Buffer.from('export default "production";');
     fs.write(fd, buf, 0, buf.length, 0, function (err, written, buffer){});
 });
 
